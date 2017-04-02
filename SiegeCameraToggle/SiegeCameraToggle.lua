3342a4434c1a0d342b7a9a1cde4d127cd9d2759a
CaptainBlagbird <CaptainBlagbird@mailueberfall.de>
2017-04-02 17:30:47 +0200

    Tabs --> Spaces

--- SiegeCameraToggle/SiegeCameraToggle.lua
+++ SiegeCameraToggle/SiegeCameraToggle.lua
@@ -12,7 +12,7 @@ SafeAddVersion("SI_BINDING_NAME_SIEGECAMERATOGGLE", 1)
 
 -- Function for binding
 function SiegeCameraToggle()
-	local setting = GetSetting(SETTING_TYPE_CAMERA, CAMERA_SETTING_THIRD_PERSON_SIEGE_WEAPONRY)
-	if setting == "1" then setting = "0" else setting = "1" end
-	SetSetting(SETTING_TYPE_CAMERA, CAMERA_SETTING_THIRD_PERSON_SIEGE_WEAPONRY, setting, 1)
+    local setting = GetSetting(SETTING_TYPE_CAMERA, CAMERA_SETTING_THIRD_PERSON_SIEGE_WEAPONRY)
+    if setting == "1" then setting = "0" else setting = "1" end
+    SetSetting(SETTING_TYPE_CAMERA, CAMERA_SETTING_THIRD_PERSON_SIEGE_WEAPONRY, setting, 1)
 end
